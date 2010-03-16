0de7452d80aaca924c572e29bcbe26aa2524b639
peter 'kovac' hausel <peter_kovac11@hotmail.com>
2010-03-16 07:05:19 -0700

    change xxxxInplace methods to xxxThis

--- src/play/db/jpa/RichModel.scala
+++ src/play/db/jpa/RichModel.scala
@@ -1,7 +1,7 @@
 package play.db.jpa
 
 class RichModel(m:Model) {
-  def saveInplace() {m.save().asInstanceOf[AnyRef]}
-  def editInplace(name:String, params:play.mvc.Scope.Params) {m.edit(name,params).asInstanceOf[AnyRef]}
-  def deleteInplace() {m.delete().asInstanceOf[AnyRef]}
+  def saveThis() {m.save().asInstanceOf[AnyRef]}
+  def editThis(name:String, params:play.mvc.Scope.Params) {m.edit(name,params).asInstanceOf[AnyRef]}
+  def deleteThis() {m.delete().asInstanceOf[AnyRef]}
 }
