bc697075b9bf2a8dd0743d32cca993cb4dd02370
pk11 <peter_kovac11@hotmail.com>
2010-05-14 00:02:07 -0400

    rearranging utils

--- src/play/utils/Utils.scala
+++ src/play/utils/Utils.scala
@@ -1,6 +1,22 @@
 package play.utils
 
 /**
+ * adding timeout handling and fixing resource closing for scala.io.Source.fromURL
+ */
+
+object Source {
+  def fromURLPath(url: String, readTimeout: Int = 5000, connectionTimeOut: Int = 3000):io.Source = {
+    import io.Source.{fromInputStream,DefaultBufSize}
+    val conn = new java.net.URL(url).openConnection()
+    conn.setReadTimeout(readTimeout)
+    conn.setConnectTimeout(connectionTimeOut)
+    val inputStream = conn.getInputStream
+    fromInputStream(inputStream,DefaultBufSize, null,() => inputStream.close())
+   }
+}
+
+
+/**
  *  based on <a href="http://www.saager.org/2007/12/30/automatic-resource-management-blocks.html">this article</a>
  * <br><br>
  *  generic ARM block to support calls like
