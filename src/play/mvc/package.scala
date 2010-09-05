af5cb79ef453c9c74c89e5f5d827c29956478400
pk11 <peter_kovac11@hotmail.com>
2010-09-04 20:06:44 -0400

    reformat code

--- src/play/mvc/package.scala
+++ src/play/mvc/package.scala
@@ -12,26 +12,26 @@ import mvc.{ScalaController, ScalaMailer, RichSession, RichResponse, RichRenderA
 
 package object mvc {
 
-        // -- HELPERS
+  // -- HELPERS
 
-        def header(name: String, value: String) = new Header(name, value)
-        def header(h: Tuple2[String, String]) = new Header(h._1, h._2)
-        
-        // -- STATUS CODE
+  def header(name: String, value: String) = new Header(name, value)
+  def header(h: Tuple2[String, String]) = new Header(h._1, h._2)
+  
+  // -- STATUS CODE
 
-        val OK              = 200
-        val CREATED         = 201
-        val ACCEPTED        = 202
-        val NO_CONTENT      = 204
-        val FORBIDDEN       = 403
-        val NOT_FOUND       = 404
-        val ERROR           = 500
+  val OK              = 200
+  val CREATED         = 201
+  val ACCEPTED        = 202
+  val NO_CONTENT      = 204
+  val FORBIDDEN       = 403
+  val NOT_FOUND       = 404
+  val ERROR           = 500
 
-        // -- TYPES REDEFINITION
+  // -- TYPES REDEFINITION
 
-        type Controller = ScalaController
-        type Mailer = ScalaMailer
+  type Controller = ScalaController
+  type Mailer = ScalaMailer
 
-    }
+  }
 
 }
