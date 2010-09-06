d12dc6727215381bb2cf1db5dcbbffc773095722
Sadek Drobi <github@sadekdrobi.com>
2010-09-06 20:56:55 +0200

    added a more generic getSync cache method

--- src/play/cache/ScalaCache.scala
+++ src/play/cache/ScalaCache.scala
@@ -55,27 +55,57 @@ private[cache] object ScalaCache extends CacheDelegate {
                 actor{link{ loop{ react{case Exit(from: Actor, exc: Exception) => {play.Logger.warn(exc.toString); from.restart()}}}}
 		      loop{
                         react{
-                          case (f:Function0[_]) => reply(f.asInstanceOf[Function0[Option[Any]]]())
+                          case (f:Function0[_]) => reply(f.asInstanceOf[Function0[Any]]())
                           case  _ => None}}}
 
    private def getFromCache[T](key:String)=Option(_impl.get(key).asInstanceOf[T])
    private def getFromCache1(key:String):Option[_]=Option(_impl.get(key))
 
-   def getAsync[T](key:String,expiration:String,window:String)(getter: => Option[T]):Option[T]={
+   def getAsync[T](key:String,expiration:String,window:String,waitForEvaluation:String="10s")(getter: => Option[T]):Option[T]={
     
-     def scheduleOrIgnore(key:String,f:Function0[_]){
+     def scheduleOrIgnore(key:String,f:Function0[_])={
+       val flagForKey="___"+key
        case class Caching() 
-       getFromCache1("___"+key).orElse
-           {val future =(cacheActor.!!( f,{
-              case v:Option[_] =>{cacheIt(v.asInstanceOf[Option[T]]); set("___"+key,None,"10s")}
-              case _ => ()
-            }))
-             None
+       getFromCache1(flagForKey).getOrElse
+           {cacheActor.!!( f,{
+              case v:Option[_] =>{cacheIt(v.asInstanceOf[Option[T]]); set(flagForKey,None,waitForEvaluation);v}
+              case _ => None
+            })
           }
    }
 
      def cacheIt(t: =>Option[T])= get(key,window,expiration)(t)
      getFromCache[T](key).orElse(getFromCache[T](prefixed(key)).map(v=>{scheduleOrIgnore(key,()=>getter);v})
 			 .orElse(cacheIt(getter))) 
+   }
+   def getAsync2[T](key:String,expiration:String,window:String,waitForEvaluation:String="10s")(getter: => Option[T]):Option[T]={
+     getAsync1[Option[T]](key,expiration,window,waitForEvaluation)(getter)(o => o.isDefined)
+   }
+   def getAsync1[A](key:String,expiration:String,window:String,waitForEvaluation:String="10s")(getter: => A)(implicit isDesirable: A => Boolean):A={
+       def scheduleOrIgnore(key:String,f:Function0[A])={
+         val flagWhileCaching="___"+key
+         case class Caching() 
+         getFromCache1(flagWhileCaching).getOrElse
+           {cacheActor.!!( ()=>{set(flagWhileCaching,Caching(),waitForEvaluation);f()},{
+             case None => None 
+             case a   =>{cacheIt(a.asInstanceOf[A]); set(flagWhileCaching,None,waitForEvaluation);Some(a)}
+            })
+          }
+       }
+
+   def cacheIt(t: =>A)= get1(key,window,expiration)(t)(isDesirable)
+   
+   getFromCache[A](key).getOrElse(
+       getFromCache[A](prefixed(key)).map(v=>{scheduleOrIgnore(key,()=>getter);v})
+			             .getOrElse(cacheIt(getter))) 
+     
+   }
+   def get1[A](key:String,window:String,expiration:String)(getter: => A)(implicit isDesirable: A => Boolean):A={
+     val cacheIt= (v:A) =>  {set(prefixed(key), v,parseDuration(expiration) + parseDuration(window) + "s" )
+                             set(key, v,expiration)
+                             v}
+     get(key).getOrElse({val result=getter;
+                         if(isDesirable(result)) cacheIt(result)
+                         else get(prefixed(key)).getOrElse(result)})
    }
 }
