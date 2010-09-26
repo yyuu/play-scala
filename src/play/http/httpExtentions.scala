f592cc9958c6b03b4c618bce02870197c7492cba
Sadek Drobi <github@sadekdrobi.com>
2010-09-26 13:54:11 +0200

    cleaned up httpExtensions and added the more flexible "as" method that takes a pattern mathicng case (2,_,_) instead of a concrete status code

--- src/play/http/httpExtentions.scala
+++ src/play/http/httpExtentions.scala
@@ -4,18 +4,32 @@ import play.libs.WS.HttpResponse
 object FunctionalWebResponse{
   implicit def httpResponse2FunctionalWebReponse(response:HttpResponse)= FunctionalWebResponse(response)
 }
-case class UndesiredStatus(statusCode: Int,body:String) {
-override def toString()="Undesired http status code "+ statusCode+":"+body
+object Status{
+  def unapply(fwr:FunctionalWebResponse):Option[(Int,Int,Int)]={
+    val status=fwr.response.getStatus().intValue;
+    Some((status / 100,status % 100 / 10,status % 10))
+  }
 }
-case class FunctionalWebResponse(response:HttpResponse){
-  def asOK():Either[UndesiredStatus,FunctionalWebResponse]=asStatus(200)
+case class UndesiredStatus(statusCode: Int,body:String) {
+  override def toString()="Undesired http status code "+ statusCode+":"+body
+}
+case class ResponseBody(private val response:HttpResponse){
   def getXml()= response.getXml()
   def getJson()= response.getJson()
   def getString()= response.getString()
-
+}
+case class FunctionalWebResponse(response:HttpResponse){
+  def body=ResponseBody(response)
+  def asOK():Either[UndesiredStatus,ResponseBody]=as{case r@Status(2,0,0)=>r.body}
   
-  //a better method would suggest a status pattern rather which is more flexible about status codes like 2xx
-  def asStatus(statusCode:Int):Either[UndesiredStatus,FunctionalWebResponse]={
-     if(response.getStatus()==statusCode) Right(this) else Left(UndesiredStatus(response.getStatus().intValue,response.getString() ))
+  def asStatus(statusCode:Int):Either[UndesiredStatus,ResponseBody]={
+    if(response.getStatus()==statusCode) 
+      Right(ResponseBody(response)) 
+    else Left(UndesiredStatus(response.getStatus().intValue,response.getString() ))
   }
+
+  def as[A](selectiveTransformer:PartialFunction[FunctionalWebResponse,A]):Either[UndesiredStatus,A]=
+    if(selectiveTransformer.isDefinedAt(this)) 
+      Right(selectiveTransformer(this)) 
+    else Left(UndesiredStatus(response.getStatus().intValue,response.getString()))
 }
