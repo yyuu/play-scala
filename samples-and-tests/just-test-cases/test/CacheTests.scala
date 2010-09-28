e2b8965cea107d28b586a77b481dd1dc02793a99
Sadek Drobi <github@sadekdrobi.com>
2010-09-28 13:50:48 +0200

    finally! testing Cache.getAsync

--- samples-and-tests/just-test-cases/test/CacheTests.scala
+++ samples-and-tests/just-test-cases/test/CacheTests.scala
@@ -33,6 +33,29 @@ class CacheTests extends UnitTestCase with ShouldMatchersForJUnit {
         Cache.get[Seq[Int]]("coco") should be (None)
         
     }
+ @Test def useTheCacheAPIAsync { 
+  
+   val workingTime=100 //time the async "job" takes to excute
+   val nbRequests=100 //number of total requests
+   val cacheTime=1000;val cacheTimePattern=cacheTime/1000+"s";
+   val tolerenceWindow=cacheTime/100+"s";
+   val nbCycles=7 //number of times cache will expire
+   val frequency=(cacheTime*nbCycles)/nbRequests //time between requests
+   val rqsForCycle=nbRequests/nbRequests //number of requests before cache expires
+   val unreachableCycles=(workingTime/frequency)/rqsForCycle //last caching cycles that won't be reached because of working time
+   case class TestCase(sent: Int,sentOn:Long,got:Int,gotOn:Long){def elapsed=gotOn-sentOn} 
+   def getInt(i:Int):Int= 
+     Cache.getAsync("coco",cacheTimePattern,tolerenceWindow){
+          Thread.sleep(workingTime); i }(_=>true)
+   def current()={System.currentTimeMillis()}
+   val result=(1 to nbRequests).map(i=> {Thread.sleep(frequency);TestCase(i,current(),getInt(i),current())})
+   
+   result.filter(_.elapsed>=workingTime).length should be (1) //only first evaluation is blocking
+   (result.map(_.got).distinct.length>unreachableCycles) should be (true) //yet the cache have been refreshed the number of reachable cycles
+   /*println(result.zip(result.tail).foldLeft(Seq.empty[String])((l,p)=>(l++Seq.fill(p._1.elapsed.intValue+1)("'"))++Seq.fill((p._2.sent-p._1.got).intValue)("_")).flatten.mkString)*/
+  
+ }
+   
     
 }
 
